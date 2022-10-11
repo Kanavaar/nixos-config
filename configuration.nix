a95467e135a1d30067129452fafd9bd83ecb1632
Kanavaar <103877398+Kanavaar@users.noreply.github.com>
2022-10-12 00:52:01 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -158,6 +158,9 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
-
+  
+  # Auto upgrading
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
 }
 
